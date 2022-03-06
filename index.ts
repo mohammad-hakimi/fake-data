a6c4d2e254c4eb4d879ba90230f9aaca256067df
Mohammad Hakimi <hakimi.mohammad.n@gmail.com>
2022-03-06 13:55:37 +0330

    fix bug

--- index.ts
+++ index.ts
@@ -2,22 +2,38 @@ import faker from '@faker-js/faker';
 import DataGen from "./types/DataGen";
 import * as fs from "fs";
 
-function generateCategory(depth: number): DataGen.Category[] {
 
-    if (depth > 0) {
-        return [{title: faker.name.title(), categories: generateCategory(depth - 1)}]
-    }
-    return [{title: faker.name.title()}]
-}
-
-function generateMainCategory(): DataGen.MainCategory {
+function generateCategory(): DataGen.Category {
 
     return {
-        _id: faker.datatype.uuid(),
-        categories: generateCategory(Math.ceil(Math.random() * 5)),
         title: faker.name.title()
     }
 }
+
+function generateCategories(depth: number): DataGen.Category[] {
+
+    if (depth > 0) {
+        const fakeData: DataGen.Category[] = []
+        for (let i = 0; i < Math.ceil(Math.random() * 5); i++) {
+            fakeData.push(generateCategory())
+        }
+        return [{title: faker.name.title(), categories: fakeData}, ...generateCategories(depth - 1)]
+    }
+    const fakeData: DataGen.Category[] = []
+    for (let i = 0; i < Math.ceil(Math.random() * 5); i++) {
+        fakeData.push(generateCategory())
+    }
+    return [{title: faker.name.title(), categories: fakeData}]
+}
+
+function generateMainCategory(): DataGen.MainCategory {
+    return {
+        _id: faker.datatype.uuid(),
+        categories: generateCategories(Math.ceil(Math.random() * 5)),
+        title: faker.name.title()
+    }
+}
+
 
 function main() {
     const fakeData: DataGen.MainCategory[] = []
